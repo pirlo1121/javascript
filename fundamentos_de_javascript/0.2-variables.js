aa42ebd79c258e7b13095bcf838b813dd5780353
maicol <pirlo1121s@gmail.com>
2026-03-25 10:12:30 -0500

    0.2 variables.js

---
+++ fundamentos_de_javascript/0.2-variables.js
@@ -0,0 +1,31 @@
+// Una variable es un espacio en memoria donde se guarda un dato 
+// para poder usarlo más tarde en el programa.
+
+// Las variables permiten:
+
+    // almacenar información
+    // reutilizar valores
+    // cambiar datos durante la ejecución del programa
+
+
+
+// En JavaScript existen tres formas de declarar variables:
+
+// var
+// let
+// const
+
+let numero1 = 22;
+const numero2 = 34;
+var numero3 = 10;
+
+
+// let y var son variables cuyo valor puedo cambiar durante la ejecución del programa.
+// Es decir, las variables numero1 y numero2 podrán tener otro valor diferente después.
+
+// const, deberá ser constante, su valor no podra cambiar, si intentamos cambiar el valor
+// de un const, nos saldrá error.
+
+numero1 = 2 // Permitido
+numero2 = 2 // Error
+numero3 = 2 // Permitido
