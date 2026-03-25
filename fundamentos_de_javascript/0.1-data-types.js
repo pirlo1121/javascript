82dbff9517f363745e2f027ef2c18077fa4e24e9
maicol <pirlo1121s@gmail.com>
2026-03-25 10:03:42 -0500

    0.1 data types v.1

---
+++ fundamentos_de_javascript/0.1-data-types.js
@@ -0,0 +1,28 @@
+// en javascript tenemos tipos de datos, son los datos que el programa va a usar o requerir para alguna funcionalidad.
+
+// tenemos 2 tipos principales ( primitivos y de referencia )
+
+// primitivos
+
+// 	son los datos mas basicos de un lenguaje de programacion
+// 	no tienen propiedades complejas.
+//  ejemplo de primitivos:
+
+// 	string: texto, palabras, frases o caracteres, los string siempre deben ir entre 
+    // comillas, ejemplo: "esto es un string"
+
+// 	number: numeros enteros o decimales, ejemplo: 35 1.8
+
+// 	boolean: lo valores booleanos solo tienen dos estados ( true o false )
+
+// 	undefined: el valor undefined es cuando una variable no se le asigno un valor, 
+    // es decir, es una variable sin definir
+
+"mango" // string
+33      // number
+false   // boolean
+
+
+// en javascript podemos ver el tipo de dato usando "typeof"
+
+console.log(typeof "mango") // en la consola saldrá ( string )
