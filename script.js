56450e086efd7b5cdc160b23656219d1dc6adcaf
maicol <pirlo1121s@gmail.com>
2026-03-25 10:38:46 -0500

    [+] structure

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log( 10 == "10" )   // true, mismo valor
+console.log( 10 === "10" )  // false, mismo valor pero diferente tipo de dato
+console.log( 10 != "10" )   // false, no son diferentes los valores
+console.log( 10 !== "10" )  // true, no son diferentes los valores pero si los tipos 
+console.log( 10 > 10 )      // false, 10 no es mayor que 10
+console.log( 10 >= 10 )     // true, no es mayor pero si es igual
+console.log( 10 < 10 )      // false, 10 no es menor que 10
+console.log( 10 <= 10 )     // true, no es menor pero si es igual
